2895a20ae1802cacf2f6be21f9ff26e07210350d
Callum Cameron <ccameron2@outlook.com>
2023-09-23 18:29:55 +0100

    update build

--- build/include/SDL3/SDL_revision.h
+++ build/include/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-3.0.0-2459-g8d49f8ffd (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-prerelease-3.0.0-2520-g92cdfbda5 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-prerelease-3.0.0-2459-g8d49f8ffd"
+#define SDL_REVISION "SDL-prerelease-3.0.0-2520-g92cdfbda5"
 #endif
 
 #endif /* SDL_revision_h_ */
