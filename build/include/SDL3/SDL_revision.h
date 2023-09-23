5a3aae97fbc15905137ea327d7603256dd8634dc
Callum Cameron <ccameron2@outlook.com>
2023-09-23 18:57:05 +0100

    cmake files

--- build/include/SDL3/SDL_revision.h
+++ build/include/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-3.0.0-2521-g2895a20ae (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-prerelease-3.0.0-2522-gfaef5daf4 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-prerelease-3.0.0-2521-g2895a20ae"
+#define SDL_REVISION "SDL-prerelease-3.0.0-2522-gfaef5daf4"
 #endif
 
 #endif /* SDL_revision_h_ */
