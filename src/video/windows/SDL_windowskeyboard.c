5e8fed6cedc42d81bf6e11caf83c5f88e392532e
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2024-06-26 00:45:05 +0100

    Reapply "Merge remote-tracking branch 'upstream/main'"
    This reverts commit 6f3f54c0f30ad83149a20e722a3f45621d6d60b1.

--- src/video/windows/SDL_windowskeyboard.c
+++ src/video/windows/SDL_windowskeyboard.c
@@ -365,7 +365,7 @@ static void UILess_DisableUIUpdates(SDL_VideoData *videodata);
 
 static SDL_bool WIN_ShouldShowNativeUI()
 {
-    return SDL_GetHintBoolean(SDL_HINT_IME_SHOW_UI, SDL_FALSE);
+    return SDL_GetHintBoolean(SDL_HINT_IME_SHOW_UI, SDL_TRUE);
 }
 
 static int IME_Init(SDL_VideoData *videodata, HWND hwnd)
@@ -839,6 +839,7 @@ static void IME_GetCompositionString(SDL_VideoData *videodata, HIMC himc, DWORD 
 
     videodata->ime_composition[length] = 0;
 
+#if 0 // At least with the Chinese IME, it's possible to move the cursor to the beginning of the selection, see https://github.com/libsdl-org/SDL/issues/9761 for details
     // Get the correct caret position if we've selected a candidate from the candidate window
     if (videodata->ime_cursor == 0 && length > 0) {
         Sint32 start = 0;
@@ -871,6 +872,7 @@ static void IME_GetCompositionString(SDL_VideoData *videodata, HIMC himc, DWORD 
 
         videodata->ime_cursor = end;
     }
+#endif // 0
 }
 
 static void IME_SendInputEvent(SDL_VideoData *videodata)
